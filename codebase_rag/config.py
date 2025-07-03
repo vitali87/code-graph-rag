34de7340d5a79f4731606a9bf52e44706738dc98
vitali87 <eheva87@gmail.com>
2025-07-04 00:01:59 +0100

    optimize: fix pydantic import

--- codebase_rag/config.py
+++ codebase_rag/config.py
@@ -1,6 +1,7 @@
 
 from dotenv import load_dotenv
-from pydantic import AnyHttpUrl, BaseSettings, model_validator
+from pydantic import AnyHttpUrl, model_validator
+from pydantic_settings import BaseSettings
 
 # Load environment variables from .env file
 # Pydantic's BaseSettings will also automatically read from .env files,
