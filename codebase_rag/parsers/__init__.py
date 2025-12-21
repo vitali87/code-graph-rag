f4b459b2aeda9bd780334d80f20d4395ca62a604
vitali87 <eheva87@gmail.com>
2025-12-21 23:31:44 +0000

    refactor(parsers): extract stdlib extraction to dedicated module

--- codebase_rag/parsers/__init__.py
+++ codebase_rag/parsers/__init__.py
@@ -2,14 +2,16 @@ from .call_processor import CallProcessor
 from .definition_processor import DefinitionProcessor
 from .factory import ProcessorFactory
 from .import_processor import ImportProcessor
+from .stdlib_extractor import StdlibExtractor
 from .structure_processor import StructureProcessor
 from .type_inference import TypeInferenceEngine
 
 __all__ = [
-    "StructureProcessor",
-    "ImportProcessor",
-    "DefinitionProcessor",
     "CallProcessor",
-    "TypeInferenceEngine",
+    "DefinitionProcessor",
+    "ImportProcessor",
     "ProcessorFactory",
+    "StdlibExtractor",
+    "StructureProcessor",
+    "TypeInferenceEngine",
 ]
