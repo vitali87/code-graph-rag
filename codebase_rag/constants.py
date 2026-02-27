f8f9c4f0b0a482d64bd136c2d28dd3781ab035da
vitali87 <eheva87@gmail.com>
2026-02-27 00:05:01 +0000

    fix: keep logfire_api in PyInstaller bundle for pydantic_graph

--- codebase_rag/constants.py
+++ codebase_rag/constants.py
@@ -883,7 +883,7 @@ PYINSTALLER_ARG_HIDDEN_IMPORT = "--hidden-import"
 PYINSTALLER_ARG_EXCLUDE_MODULE = "--exclude-module"
 PYINSTALLER_ENTRY_POINT = "main.py"
 
-PYINSTALLER_EXCLUDED_MODULES = ["logfire", "logfire_api"]
+PYINSTALLER_EXCLUDED_MODULES = ["logfire"]
 
 # (H) TOML parsing constants
 TOML_KEY_PROJECT = "project"
