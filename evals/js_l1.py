c5388f4572ebc404eca3fe3671db9ea30ea8405c
vitali87 <eheva87@gmail.com>
2026-06-21 18:56:31 +0100

    feat(evals): grade JavaScript node spans (end_line) against the tsc oracle

--- evals/js_l1.py
+++ evals/js_l1.py
@@ -40,7 +40,9 @@ def main(
     oracle = run_javascript_oracle(target)
     logger.success(ls.JS_ORACLE_DONE.format(count=len(oracle.nodes)))
 
-    result = score_structure(cgr, oracle, ec.JS_SCORED_NODE_KINDS, ec.SCORED_EDGE_TYPES)
+    result = score_structure(
+        cgr, oracle, ec.JS_SCORED_NODE_KINDS, ec.SCORED_EDGE_TYPES, grade_spans=True
+    )
     write_outputs(result, out_dir, ec.JS_SCORES_FILENAME, ec.JS_DIFF_FILENAME)
     render(result, _TITLE)
 
